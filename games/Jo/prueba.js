05670a831a61533ebcc01472692ce7ee4d970d98
LaPandillaProducciones <127466843+LaPandillaProducciones@users.noreply.github.com>
2023-05-04 09:16:16 -0600

    actualizacion pruebas

---
+++ games/Jo/prueba.js
@@ -0,0 +1,37 @@
+var testPosRaton = document.getElementById("testPosRaton");
+var output = document.getElementById("output");
+
+testPosRaton.addEventListener("mousemove", function(evt) {
+  var mousePos = oMousePos(testPosRaton, evt);
+  marcarCoords(output, mousePos.x, mousePos.y)
+}, false);
+
+testPosRaton.addEventListener("mouseout", function(evt) {
+  limpiarCoords(output);
+}, false);
+
+function marcarCoords(output, x, y) {
+  output.innerHTML = ("x: " + x + ", y: " + y);
+  var cssString = "";
+  // http://www.quirksmode.org/dom/w3c_css.html#t30
+  cssString += "top:" + (y + 10) + "px;";
+  cssString += "left:" + (x + 10) + "px;";
+  cssString += "visibility:visible;";
+  output.style.cssText = cssString;
+
+  testPosRaton.style.cursor = "pointer";
+}
+
+function limpiarCoords(output) {
+  output.style.cssText = "";
+  testPosRaton.style.cursor = "default";
+}
+
+function oMousePos(element, evt) {
+  var ClientRect = element.getBoundingClientRect();
+  return { //objeto
+    x: Math.round(evt.clientX - ClientRect.left),
+    y: Math.round(evt.clientY - ClientRect.top)
+  }
+}
+
